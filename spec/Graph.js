25708f561c3096e135127576406acdc98554c196
Henri Bergius <henri.bergius@iki.fi>
2020-11-27 09:34:33 +0100

    Simplify noflo-wrapper usage and capture Flowtraces

--- spec/Graph.js
+++ spec/Graph.js
@@ -1,31 +1,21 @@
 /* eslint-env node, mocha */
 const { expect } = require('chai');
 const Wrapper = require('noflo-wrapper');
-const { ComponentLoader } = require('noflo');
 const { resolve } = require('path');
 
 describe('Assembly Graph', function bar() {
   this.timeout(5000);
-  let loader;
   let c;
-  before((done) => {
-    // A bit of magic with custom loader for subfolder project to work
-    loader = new ComponentLoader(resolve(__dirname, '../example'));
-    loader.listComponents((err) => {
-      if (err) { done(err); return; }
-      done();
-    });
-  });
 
   describe('A simple pipeline graph', () => {
     before((done) => {
-      c = new Wrapper('', { loader });
-      c.component = 'example/BuildChassis';
-      c.start((err) => {
-        if (err) { done(err); return; }
-        done();
+      c = new Wrapper('example/BuildChassis', {
+        baseDir: resolve(__dirname, '../example'),
+        debug: true,
       });
+      c.start(done);
     });
+    after(() => c.dumpTrace());
 
     it('should build a car chassis', (done) => {
       c.receive('out', (msg) => {
@@ -55,13 +45,13 @@ describe('Assembly Graph', function bar() {
 
   describe('A graph with branches', () => {
     before((done) => {
-      c = new Wrapper('', { loader });
-      c.component = 'example/BuildBody';
-      c.start((err) => {
-        if (err) { done(err); return; }
-        done();
+      c = new Wrapper('example/BuildBody', {
+        baseDir: resolve(__dirname, '../example'),
+        debug: true,
       });
+      c.start(done);
     });
+    after(() => c.dumpTrace());
 
     it('should build a car body', (done) => {
       c.receive('out', (msg) => {
@@ -89,13 +79,13 @@ describe('Assembly Graph', function bar() {
 
   describe('A top level graph with subgraphs', () => {
     before((done) => {
-      c = new Wrapper('', { loader });
-      c.component = 'example/BuildCar';
-      c.start((err) => {
-        if (err) { done(err); return; }
-        done();
+      c = new Wrapper('example/BuildCar', {
+        baseDir: resolve(__dirname, '../example'),
+        debug: true,
       });
+      c.start(done);
     });
+    after(() => c.dumpTrace());
 
     it('should build a car', (done) => {
       c.receive('out', (msg) => {
